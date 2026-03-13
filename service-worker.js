af4e4840ae41980a7f2a74eb7e103214270d1e21
BraianR <braianruaimi@gmail.com>
2026-03-13 00:56:19 -0300

    Crear landing PWA mobile-first de Violencia Invisible

---
+++ service-worker.js
@@ -0,0 +1,48 @@
+const VERSION = "vi-pwa-v1";
+const ARCHIVOS_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (evento) => {
+  evento.waitUntil(
+    caches.open(VERSION).then((cache) => cache.addAll(ARCHIVOS_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (evento) => {
+  evento.waitUntil(
+    caches.keys().then((claves) =>
+      Promise.all(
+        claves
+          .filter((clave) => clave !== VERSION)
+          .map((clave) => caches.delete(clave))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (evento) => {
+  if (evento.request.method !== "GET") {
+    return;
+  }
+
+  evento.respondWith(
+    caches.match(evento.request).then((enCache) => {
+      if (enCache) {
+        return enCache;
+      }
+
+      return fetch(evento.request)
+        .then((respuestaRed) => {
+          const copia = respuestaRed.clone();
+          caches.open(VERSION).then((cache) => cache.put(evento.request, copia));
+          return respuestaRed;
+        })
+        .catch(() => caches.match("./index.html"));
+    })
+  );
+});
